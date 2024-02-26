a84cbec5241523b45ae0f688bdb1ac59aabb705f
izabelle-g <aguev976@mtroyal.ca>
2024-02-26 06:21:43 -0700

    Trying to make hosting work part 5

--- scripts/f1-router.js
+++ scripts/f1-router.js
@@ -24,7 +24,7 @@ const handleAllSeasons = (app) => {
 
 // Route 2: Returns all the data in the circuits table
 const handleAllCircuits = (app) => {
-    app.get('https://w2024-assign1-8plk.onrender.com/api/circuits', async (req, res) => {
+    app.get('/api/circuits', async (req, res) => {
         const {data, error} = await supabase
         .from('circuits')
         .select();
@@ -36,7 +36,7 @@ const handleAllCircuits = (app) => {
 
 // Route 3: Returns the specified circuit
 const handleCircuit = (app) => {
-    app.get('https://w2024-assign1-8plk.onrender.com/api/circuits/:ref', async (req, res) => {
+    app.get('/api/circuits/:ref', async (req, res) => {
         const {data, error} = await supabase
         .from('circuits')
         .select()
@@ -52,7 +52,7 @@ const handleCircuit = (app) => {
 
 // Route 4: Returns the circuits in a given season
 const handleAllCircuitsInSeason = (app) => {
-    app.get('https://w2024-assign1-8plk.onrender.com/api/circuits/seasons/:year', async (req, res) => {
+    app.get('/api/circuits/seasons/:year', async (req, res) => {
         const {data, error} = await supabase
         .from('races')
         .select(`name, round, seasons!inner (year), circuits (name, location, country)`)
@@ -69,7 +69,7 @@ const handleAllCircuitsInSeason = (app) => {
 
 // Route 5: Returns all the constructors
 const handleAllConstructors = (app) => {
-    app.get('https://w2024-assign1-8plk.onrender.com/api/constructors', async (req, res) => {
+    app.get('/api/constructors', async (req, res) => {
         const {data, error} = await supabase
         .from('constructors')
         .select();
@@ -81,7 +81,7 @@ const handleAllConstructors = (app) => {
 
 // Route 6: Returns just the specified constructor
 const handleConstructor = (app) => {
-    app.get('https://w2024-assign1-8plk.onrender.com/api/constructors/:ref', async (req, res) => {
+    app.get('/api/constructors/:ref', async (req, res) => {
         const {data, error} = await supabase
         .from('constructors')
         .select()
@@ -97,7 +97,7 @@ const handleConstructor = (app) => {
 
 // Route 7: Returns all the drivers
 const handleAllDrivers = (app) => {
-    app.get('https://w2024-assign1-8plk.onrender.com/api/drivers', async (req, res) => {
+    app.get('/api/drivers', async (req, res) => {
         const {data, error} = await supabase
         .from('drivers')
         .select();
@@ -109,7 +109,7 @@ const handleAllDrivers = (app) => {
 
 // Route 8: Returns just the specified driver
 const handleDriver = (app) => {
-    app.get('https://w2024-assign1-8plk.onrender.com/api/drivers/:ref', async (req, res) => {
+    app.get('/api/drivers/:ref', async (req, res) => {
         const {data, error} = await supabase
         .from('drivers')
         .select()
@@ -125,7 +125,7 @@ const handleDriver = (app) => {
 
 // Route 9: Returns the drivers whose surname (case insensitive) begins with the provided prefix
 const handleDriversWithPrefix = (app) => {
-    app.get('https://w2024-assign1-8plk.onrender.com/api/drivers/search/:substring', async (req, res) => {
+    app.get('/api/drivers/search/:substring', async (req, res) => {
         const {data, error} = await supabase
         .from('drivers')
         .select()
@@ -142,7 +142,7 @@ const handleDriversWithPrefix = (app) => {
 
 // Route 10: Returns the drivers within a given race
 const handleDriversInRace = (app) => {
-    app.get('https://w2024-assign1-8plk.onrender.com/api/drivers/race/:raceId', async (req, res) => {
+    app.get('/api/drivers/race/:raceId', async (req, res) => {
         const {data, error} = await supabase
         .from('results')
         .select(`races!inner (raceId, name, year), drivers (*)`)
@@ -158,7 +158,7 @@ const handleDriversInRace = (app) => {
 
 // Route 11: Returns just the specified race
 const handleRace = (app) => {
-    app.get('https://w2024-assign1-8plk.onrender.com/api/races/:raceId', async (req, res) => {
+    app.get('/api/races/:raceId', async (req, res) => {
         const {data, error} = await supabase
         .from('races')
         .select(`name, year, circuits!inner (name, location, country)`)
@@ -174,7 +174,7 @@ const handleRace = (app) => {
 
 // Route 12: Returns the races within a given season
 const handleRacesInSeason = (app) => {
-    app.get('https://w2024-assign1-8plk.onrender.com/api/races/season/:year', async (req, res) => {
+    app.get('/api/races/season/:year', async (req, res) => {
         const {data, error} = await supabase
         .from('races')
         .select('*, seasons!inner (*)')
@@ -191,7 +191,7 @@ const handleRacesInSeason = (app) => {
 
 // Route 13: Returns a specific race within a given season specified by the round number
 const handleNthRaceInSeason = (app) => {
-    app.get('https://w2024-assign1-8plk.onrender.com/api/races/season/:year/:round', async (req, res) => {
+    app.get('/api/races/season/:year/:round', async (req, res) => {
         const {data, error} = await supabase
         .from('races')
         .select('*, seasons!inner (*)')
@@ -211,7 +211,7 @@ const handleNthRaceInSeason = (app) => {
 
 // Route 14: Returns all the races for a given circuit
 const handleRacesInCircuit = (app) => {
-    app.get('https://w2024-assign1-8plk.onrender.com/api/races/circuits/:ref', async (req, res) => {
+    app.get('/api/races/circuits/:ref', async (req, res) => {
         const {data, error} = await supabase
         .from('races')
         .select(`name, year, circuits!inner (name, location, country)`)
@@ -228,7 +228,7 @@ const handleRacesInCircuit = (app) => {
 
 // Route 15: Returns all the races for a given circuit between two years
 const handleRacesInCircuitBetween = (app) => {
-    app.get('https://w2024-assign1-8plk.onrender.com/api/races/circuits/:ref/season/:start/:end', async (req, res) => {
+    app.get('/api/races/circuits/:ref/season/:start/:end', async (req, res) => {
         const {data, error} = await supabase
         .from('races')
         .select(`name, year, circuits!inner (name, location, country)`)
@@ -246,7 +246,7 @@ const handleRacesInCircuitBetween = (app) => {
 
 // Route 16: Returns the results for the specified race
 const handleResultsforRace = (app) => {
-    app.get('https://w2024-assign1-8plk.onrender.com/api/results/:raceId', async (req, res) => {
+    app.get('/api/results/:raceId', async (req, res) => {
         const {data, error} = await supabase
         .from('results')
         .select(`drivers!inner (driverRef, code, forename, surname), races!inner (name, round, year, date), constructors!inner (name, constructorRef, nationality)`)
@@ -261,7 +261,7 @@ const handleResultsforRace = (app) => {
 
 // Route 17: Returns all the results for a given driver
 const handleDriverResults = (app) => {
-    app.get('https://w2024-assign1-8plk.onrender.com/api/results/driver/:ref', async (req, res) => {
+    app.get('/api/results/driver/:ref', async (req, res) => {
         const {data, error} = await supabase
         .from('results')
         .select('drivers!inner (driverRef, code, forename, surname), *')
@@ -275,7 +275,7 @@ const handleDriverResults = (app) => {
 
 // Route 18: Returns all the results for a given driver between two years
 const handleDriverResultsBetween = (app) => {
-    app.get('https://w2024-assign1-8plk.onrender.com/api/results/driver/:ref/seasons/:start/:end', async (req, res) => {
+    app.get('/api/results/driver/:ref/seasons/:start/:end', async (req, res) => {
         const {data, error} = await supabase
         .from('results')
         .select(`drivers!inner (driverRef, code, forename, surname), races!inner (year), *`)
@@ -292,7 +292,7 @@ const handleDriverResultsBetween = (app) => {
 
 // Route 19: Returns the qualifying results for the specified race
 const handleRaceQualResults = (app) => {
-    app.get('https://w2024-assign1-8plk.onrender.com/api/qualifying/:raceId', async (req, res) => {
+    app.get('/api/qualifying/:raceId', async (req, res) => {
         const {data, error} = await supabase
         .from('qualifying')
         .select(`drivers!inner (driverRef, code, forename, surname), races!inner (name, round, year, date), constructors!inner (name, constructorRef, nationality)`)
@@ -307,7 +307,7 @@ const handleRaceQualResults = (app) => {
 
 // Route 20: Returns the current season driver standings table for the specified race
 const handleSeasonStandings = (app) => {
-    app.get('https://w2024-assign1-8plk.onrender.com/api/standings/:raceId/drivers', async (req, res) => {
+    app.get('/api/standings/:raceId/drivers', async (req, res) => {
         const {data, error} = await supabase
         .from('driverStandings')
         .select(`drivers!inner (driverRef, code, forename, surname), races!inner (name, round, year, date)`)
@@ -322,7 +322,7 @@ const handleSeasonStandings = (app) => {
 
 // Route 21: Returns the current season constructors standings table for the specified race
 const handleConstructorStandings = (app) => {
-    app.get('https://w2024-assign1-8plk.onrender.com/api/standings/:raceId/constructors', async (req, res) => {
+    app.get('/api/standings/:raceId/constructors', async (req, res) => {
         const {data, error} = await supabase
         .from('constructorStandings')
         .select(`constructors!inner (constructorRef, name, nationality), races!inner (name, round, year, date)`)
